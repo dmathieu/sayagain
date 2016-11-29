972714fac0e34d4ed04c2e678de0d736432d67c0
Damien Mathieu <damien@heroku.com>
2016-11-29 11:48:21 +0100

    fix common test

--- test/keys_SUITE.erl
+++ test/keys_SUITE.erl
@@ -5,37 +5,21 @@
 all() -> 
   [set_and_get].
 
-init_per_suite(Config) ->
-  NewConfig = start_server(Config),
-  {ok, Client} = connect_erldis(10),
-  lists:keystore(client, 1, NewConfig, {client, Client}).
-
-init_per_testcase(_TestCase,Config) ->
-  {client,Client} = lists:keyfind(client, 1, Config),
-  erldis_client:sr_scall(Client,[<<"flushall">>]),
-  Config.
-
-end_per_suite(Config) ->
-  stop_server(Config),
-  ok.
-
-start_server(Config) ->
+start_server() ->
   application:set_env(sayagain, address, localhost),
-  application:set_env(sayagain, port, 5000),
+  application:set_env(sayagain, port, 1304),
   process_flag(trap_exit, true),
-  {ok, Pid} = say_sup:start_link(),
-  lists:keystore(server,1, Config, {server, Pid}).
+  say_sup:start_link().
 
-stop_server(Config) ->
+stop_server(Pid) ->
   application:unset_env(sayagain, address),
   application:unset_env(sayagain, port),
-  {server, Pid} = lists:keyfind(server, 1, Config),
   exit(Pid, kill). %% brutal kill!
 
 connect_erldis(0) -> {error,{socket_error,econnrefused}};
 connect_erldis(Times) ->
   timer:sleep(2000),
-  case erldis:connect(localhost, 5000) of
+  case erldis:connect(localhost, 1304) of
     {ok,Client} -> {ok, Client};
     _ -> connect_erldis(Times - 1)
   end.
@@ -44,9 +28,14 @@ connect_erldis(Times) ->
 %% TESTS %%
 %%%%%%%%%%%
 
-set_and_get(Config)->
-  {client,Client} = lists:keyfind(client, 1, Config),
+set_and_get(_Config)->
+  {ok, Pid} = start_server(),
+  {ok, Client} = connect_erldis(10),
+  ok = erldis_client:sr_scall(Client, [<<"flushall">>]),
+
   ok = erldis_client:sr_scall(Client, [<<"set">>,<<"string">>,<<"foo">>]),
   ok = erldis_client:sr_scall(Client, [<<"set">>,<<"string">>,<<"bar">>]),
   <<"bar">> = erldis_client:sr_scall(Client, [<<"get">>,<<"string">>]),
-  nil = erldis_client:sr_scall(Client, [<<"get">>,<<"string2">>]).
+  nil = erldis_client:sr_scall(Client, [<<"get">>,<<"string2">>]),
+
+  stop_server(Pid).
